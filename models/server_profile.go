93055f044d80c3abebe9bc462269428f7dbb7c49
Mingjie Li <limingjie@outlook.com>
2017-03-10 11:35:34 +0800

    Update Siebel Server Profile

--- models/server_profile.go
+++ models/server_profile.go
@@ -3,9 +3,22 @@ package models
 type (
 	// ServerConfigParams struct
 	ServerConfigParams struct {
-		SiebelServer     string `json:"SiebelServer"`
-		SiebelServerDesc string `json:"SiebelServerDesc"`
-		DeployedLanguage string `json:"DeployedLanguage"`
+		AnonLoginUserName          string `json:"AnonLoginUserName"`
+		AnonLoginPassword          string `json:"AnonLoginPassword"`
+		EnableCompGroupsSIA        string `json:"EnableCompGroupsSIA"`
+		SCBPort                    string `json:"SCBPort"`
+		LocalSynchMgrPort          string `json:"LocalSynchMgrPort"`
+		ModifyServerEncrypt        bool   `json:"ModifyServerEncrypt"`
+		ModifyServerAuth           bool   `json:"ModifyServerAuth"`
+		ClusteringEnvironmentSetup string `json:"ClusteringEnvironmentSetup"`
+		SiebelClusterGateway       string `json:"SiebelClusterGateway"`
+		UseOracleConnector         bool   `json:"UseOracleConnector"`
+		Encrypt                    string `json:"Encrypt"`
+		CACertFileName             string `json:"CACertFileName"`
+		CertFileNameServer         string `json:"CertFileNameServer"`
+		Username                   string `json:"Username"`
+		Password                   string `json:"Password"`
+		SQLServerPort              string `json:"SqlServerPort"`
 	}
 
 	// ServerProfile struct
